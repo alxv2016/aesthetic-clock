e775d43e7cb4a07914b1e5bc3c4cd5b9103ac18a
Alex Vong <avong2011@gmail.com>
2022-02-27 17:41:36 -0500

    feat: added time animation

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -24,11 +24,12 @@ export class ClockComponent implements OnInit, AfterViewInit {
   minutes: string | undefined = '00';
   hours: string | undefined = '00';
   todaysDate: string | undefined = 'Friday, January 16 2022';
-  meridian: string | undefined = 'am';
+  meridian: string | undefined = 'AM';
   @HostBinding('class') class = 'c-clock';
-  @ViewChild('secondsDigit') secondsDigit!: ElementRef;
-  @ViewChild('minutesDigit') minutesDigit!: ElementRef;
+  // @ViewChild('secondsDigit') secondsDigit!: ElementRef;
+  // @ViewChild('minutesDigit') minutesDigit!: ElementRef;
   @ViewChildren('digit', {read: ElementRef}) digit!: QueryList<ElementRef>;
+  @ViewChildren('colon', {read: ElementRef}) colon!: QueryList<ElementRef>;
   constructor(private element: ElementRef, private render: Renderer2, private ngZone: NgZone) {
     gsap.registerPlugin(ScrollTrigger);
   }
@@ -38,43 +39,57 @@ export class ClockComponent implements OnInit, AfterViewInit {
     this.todaysDate = humanDate.format('dddd, MMMM DD YYYY');
   }
 
-  private initTime(gsapTimeLine: GSAPTimeline): void {
-    const now = moment();
-    const percent = Number(this.seconds) / 4;
-    this.seconds = now.format('ss');
-    this.minutes = now.format('mm');
-    this.hours = now.format('hh');
-    this.meridian = now.format('a');
-    // const digits = this.digit.map((d) => d.nativeElement);
-    // gsapTimeLine
-    //   .to(digits[0], {
-    //     xPercent: -percent,
-    //   })
-    //   .to(
-    //     digits[1],
-    //     {
-    //       xPercent: percent,
-    //     },
-    //     0
-    //   )
-    //   .to(
-    //     digits[2],
-    //     {
-    //       xPercent: -percent,
-    //     },
-    //     0
-    //   );
-  }
-
   private initGSAP(): void {
-    const cloclTL = gsap.timeline({
+    const colons = this.colon.map((c) => c.nativeElement);
+    const digits = this.digit.map((d) => d.nativeElement);
+    const clockTL = gsap.timeline({
       defaults: {
-        ease: 'back',
+        ease: 'power3',
       },
     });
 
+    gsap.fromTo(
+      colons,
+      {
+        scale: 0.45,
+      },
+      {
+        scale: 1,
+        ease: 'power3',
+        transformOrigin: 'center',
+        repeat: -1,
+        yoyo: true,
+      }
+    );
+
+    let minuteCount = 0;
+
     setInterval(() => {
-      this.initTime(cloclTL);
+      const momentum = 12;
+      const now = moment();
+      const count = Number(this.seconds);
+      if (count >= 59) {
+        minuteCount += momentum;
+      }
+      if (minuteCount >= 60) {
+        minuteCount = 0;
+      }
+
+      this.seconds = now.format('ss');
+      this.minutes = now.format('mm');
+      this.hours = now.format('hh');
+      this.meridian = now.format('A');
+
+      clockTL
+        .to(digits[1], {
+          xPercent: Math.floor(Math.sin(minuteCount) * momentum),
+          yPercent: Math.floor(Math.cos(minuteCount) * momentum),
+        })
+        .to(digits[2], {
+          xPercent: Math.floor(Math.sin(count) * momentum),
+          yPercent: Math.floor(Math.sin(count) * momentum) * -1,
+          ease: 'power4',
+        });
     }, 1000);
   }
 
